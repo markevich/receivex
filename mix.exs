f8afbbc551cb4f001ca1cb7bd1a2aac78ae12e4f
markevich <slava.markevich@gmail.com>
2020-05-24 15:49:40 +0300

    update package version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Receivex.MixProject do
   def project do
     [
       app: :receivex,
-      version: "0.8.1",
+      version: "0.8.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
